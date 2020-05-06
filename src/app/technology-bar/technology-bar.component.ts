08d105cc543af7cf8da0cfb4f7106eb07422d0df
DamiEn83x <damian_dur@poczta.fm>
2020-05-06 15:42:32 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/technology-bar/technology-bar.component.ts
+++ src/app/technology-bar/technology-bar.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-technology-bar',
   templateUrl: './technology-bar.component.html',
   styleUrls: ['./technology-bar.component.css']
 })
 export class TechnologyBarComponent implements OnInit {
-private LOGOAngular = require("./assets/logo.png");
+
   constructor() { }
 
   ngOnInit() {
