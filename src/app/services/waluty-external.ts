6f8b58632d83f69c4415718a20c2a39645afce20
DamiEn83x <damian_dur@poczta.fm>
2020-07-16 21:28:29 +0200

    1 file modified

--- src/app/services/waluty-external.ts
+++ src/app/services/waluty-external.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders,HttpResponse} from '@angular/common/http';
 const CURR_SERVICE_API:string= 'https://currencyservice.damiand1.repl.co';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  withCredentials: false,
+  observe: 'response' as 'response'
+};
 @Injectable()
 export default class WalutyExternal {
 
@@ -16,8 +21,10 @@ export default class WalutyExternal {
       {
         let url=CURR_SERVICE_API+'/?query=GettabelaWalutA';
         console.log(url);
-         this.http.get<any>(url).subscribe((res)=>
+         this.http.get<any>(url,httpOptions).subscribe((res:HttpResponse<any>)=>
          { console.log(res);   
+         console.log('response from server:',res);
+  console.log('response headers',res.headers.keys())
            let out= res[0]['rates'].map((rate)=>
               {
                 return { 
@@ -39,20 +46,15 @@ export default class WalutyExternal {
       
          let url=CURR_SERVICE_API;
          console.log(url);
-         let httpOptions = {
-            headers: new HttpHeaders({ 
-              'Access-Control-Allow-Headers':"Origin, X-Requested-With, Content-Type, Accept"
-            })
-        };
         let Token = Math.round(Math.random()*10000);
-         this.http.post<any>(url,{
+     this.http.post<any>(url,{
                                     "Query":"GetCurrencyPowerChanges",
                                     "DayFrom": DayFrom,
                                     "DayTo": DayTo,
                                     "tabelaWalut":JSON.stringify(tabelaWalut),
                                     "Curr": cur,
                                     "Token":Token
-                                 }).subscribe((res)=>{    
+                                 },httpOptions).subscribe((res)=>{    
            
            let tabelaZbiorcza=new Object();
         //   console.log(res);
